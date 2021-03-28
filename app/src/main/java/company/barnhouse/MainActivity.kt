bac8676d6d05427a8e28d205e29d224df5f38cce
sajka <Sudasa1989>
2021-03-28 21:09:28 +0300

    Add popupMenu code

--- app/src/main/java/company/barnhouse/MainActivity.kt
+++ app/src/main/java/company/barnhouse/MainActivity.kt
@@ -1,10 +1,12 @@
 package company.barnhouse
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -27,5 +29,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.popup_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.project ->Log.v("TAG", "project")
+            R.id.service -> Log.v("TAG", "service")
+            R.id.work -> Log.v("TAG", "work")
+            R.id.about -> Log.v("TAG", "about")
+            R.id.contacts -> Log.v("TAG", "contacts")
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
